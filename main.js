9120f8eaaca82ac2e70ad0956f28b2f1eca09718
amrkallas <amrkallas6@gmail.com>
2023-11-10 21:12:17 +0300

    two img in header

--- main.js
+++ main.js
@@ -1,6 +1,14 @@
-let theme=document.querySelector(".theme");
+let theme = document.querySelector(".theme");
+let imgElement = document.querySelector(".heading .image .second-img");
 
-theme.addEventListener("click",(e) => {
-    let element =document.body;
-    element.classList.toggle('light');
-})
+theme.addEventListener("click", (e) => {
+  let element = document.body;
+  element.classList.toggle("light");
+
+  if (element.classList.contains("light")) {
+    imgElement.style.opacity = 1;
+  } else {
+    imgElement.style.opacity = 0;
+
+  }
+});
