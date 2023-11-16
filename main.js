a2fd78f636ef0f55595dcfeed0aa172143d0b5b9
amrkallas <amrkallas6@gmail.com>
2023-11-16 22:14:53 +0300

    edit::fix layout in light mode

--- main.js
+++ main.js
@@ -1,14 +1,18 @@
 let theme = document.querySelector(".theme");
 let imgElement = document.querySelector(".heading .image .second-img");
+let layout=document.querySelector(".layout")
 
+console.log(layout)
 theme.addEventListener("click", (e) => {
   let element = document.body;
   element.classList.toggle("light");
 
   if (element.classList.contains("light")) {
     imgElement.style.opacity = 1;
+    layout.style.background='none';
   } else {
     imgElement.style.opacity = 0;
+    layout.style.background= 'rgb(0 0 0/60%)';
 
   }
 });
