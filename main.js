2fc3cd500296f63a660f46a87ac1efba17e29242
Abdulrahman_Ka <abode.kalls@gmail.com>
2023-12-19 01:46:32 +0300

    Added leader's cards, photos, and leader's footer

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 let theme = document.querySelector(".theme");
 let imgElement = document.querySelector(".heading .image .second-img");
 let layout=document.querySelector(".layout")
-let button1 = document.querySelector("#A");
-let button2 = document.querySelector("#B");
-let button3 = document.querySelector("#C");
+let button1 = document. querySelector("#leader1");
+let button2 = document.querySelector("#leader2");
+let button3 = document.querySelector("#leader3");
 console.log(layout)
 theme.addEventListener("click", (e) => {
   let element = document.body;
@@ -19,9 +19,15 @@ theme.addEventListener("click", (e) => {
   }
 });
 
+button1.addEventListener("click", (e) =>{
+  let imgs = document.querySelector("#leaderImage");
+  imgs.src = "img/amr.jpg";
+});
 button2.addEventListener("click", (e) =>{
-  let imgs = document.getElementsByClassName("active");
-  imgs.classList.toggle("active"); 
-  let img = document.querySelector("#B-image");
-  img.classList.add("active");
-})
+  let imgs = document.querySelector("#leaderImage");
+  imgs.src = "img/amr2.jpg";
+});
+button3.addEventListener("click", (e) =>{
+  let imgs = document.querySelector("#leaderImage");
+  imgs.src = "img/amr3.jpg";
+});
