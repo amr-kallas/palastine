f9bff5f5e1c7c88b70197c7377f9b7ccc1ce66de
Abdulrahman_Ka <abode.kalls@gmail.com>
2023-12-18 21:48:18 +0300

    added leaders and style them

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 let theme = document.querySelector(".theme");
 let imgElement = document.querySelector(".heading .image .second-img");
 let layout=document.querySelector(".layout")
-
+let button1 = document.querySelector("#A");
+let button2 = document.querySelector("#B");
+let button3 = document.querySelector("#C");
 console.log(layout)
 theme.addEventListener("click", (e) => {
   let element = document.body;
@@ -16,3 +18,10 @@ theme.addEventListener("click", (e) => {
 
   }
 });
+
+button2.addEventListener("click", (e) =>{
+  let imgs = document.getElementsByClassName("active");
+  imgs.classList.toggle("active"); 
+  let img = document.querySelector("#B-image");
+  img.classList.add("active");
+})
